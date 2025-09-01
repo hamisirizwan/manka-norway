1d3c46a43467b174d2ee0859ad9f230972e07c66
hamisirizwan <hamisirizwan5@gmail.com>
2025-09-01 22:35:49 +0300

    assignment added

---
+++ lesson6/test/balance.js
@@ -0,0 +1,9 @@
+// create a user object
+
+// the user object should have name , initial_deposit properties
+
+// the user object should also contain a transactions array of object with type and value
+
+// write code to get the user balance
+
+// if the balance is negative then console that the user has an overdraft of the balance , else just console the balance
