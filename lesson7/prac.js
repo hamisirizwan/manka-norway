5c5ca6e7540f0522e0fe4c1b03787038af784698
amaan dheere <namsos15outlook.com>
2025-09-10 20:30:27 +0200

    solution of function assignment

--- lesson7/prac.js
+++ lesson7/prac.js
@@ -26,22 +26,32 @@ const school = {
   ],
 };
 
-console.log(
-  `${school.name} is located in ${school.location} has ${school.teachers.length} teachers and ${school.students.length} students`
-);
-
+// console.log(
+//   `${school.name} is located in ${school.location} has ${school.teachers.length} teachers and ${school.students.length} students`
+// );
 
 // create a function that will rename the school
 
-function renameSchool(newName){
-  console.log(`.....renaming the school from ${school.name} to ${newName}.....`)
-  school.name = newName
+function renameSchool(newName) {
+  console.log(
+    `.....renaming the school from ${school.name} to ${newName}.....`
+  );
+  school.name = newName;
 }
 
-renameSchool("Mangu")
+// renameSchool("Mangu");
 
-console.log(
-  `${school.name} is located in ${school.location} has ${school.teachers.length} teachers and ${school.students.length} students`
-);
+// console.log(
+// `${school.name} is located in ${school.location} has ${school.teachers.length} teachers and ${school.students.length} students`
+// );
 
 // create a function that will console the total grade of the students in the school
+
+function totalGrade() {
+  let total = 0;
+  for (let i = 0; i < school.students.length; i++) {
+    totalGrade = total + school.students.length;
+  }
+  console.log(total)
+}
+totalGrade();
