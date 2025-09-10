93dbcb07103d6cb5b00527dc0f5d71d589dfc13b
hamisirizwan <hamisirizwan5@gmail.com>
2025-09-10 22:50:12 +0300

    intro to object methods

---
+++ lesson10/mathobject.js
@@ -0,0 +1,35 @@
+// create an object that has 2 numbers and methods to add , subract , multiply
+
+// string , boolean , number , object  , array
+// functions
+const mathObject = {
+  num1: 30,
+  num2: 10,
+  add() {
+    console.log(this.num1 + this.num2);
+  },
+  subtract() {
+    console.log(this.num1 - this.num2);
+  },
+  multiply() {
+    console.log(this.num1 * this.num2);
+  },
+};
+
+mathObject.add();
+mathObject.subtract();
+mathObject.multiply();
+
+// console.log(Math.ceil(Math.random()))
+
+// Array object
+// ceil , random , floor , abs , PI ,
+
+// DOM - getElementById
+
+// LocalStorage - localStorage.get() , localstroge.set()
+
+// REACT - react.useState() , react.useEffect()
+// NODE  - app.router()
+// array.map() , array.filter() , array.find() , array.reduce() , array.forEach()
+// string.toUpperCase , string.substring() , string.concat , string.split() , string.strip()
---
+++ lesson10/objectmethod.js
@@ -0,0 +1,26 @@
+// a function is basically another datatype
+
+// const addFunction =
+// addFunction()
+
+// if a object can hold strings and number - they can hold functions
+
+const mathematics = {
+  addFunction () {
+    console.log(1 + 1);
+  },
+
+  name: "Hello",
+
+  subract(){
+
+  }
+  ,
+  multiply(){
+    
+  }
+};
+
+mathematics.addFunction()
+
+
